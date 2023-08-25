2349bb9ecfba5f0efa02ba90ca60f81790cdfd33
Abhishek Jain <abhishek.j0407@gmail.com>
2023-08-25 17:51:35 +0530

    Hero section added, styled

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -1,9 +1,30 @@
-import './Hero.css'
+import "./Hero.css";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section id="hero">
+      <div className="container">
+        <div className="left">
+          <div className="content">
+            <h1>Get an Instant offer and sell your home</h1>
+            <form action="#" className="offer-form">
+              <img src="/assets/homepage/map.svg" alt="map" />
+              <input
+                type="text"
+                placeholder="Where's your house?"
+                className="input"
+              />
+              <button type="submit">Get my free offer</button>
+            </form>
+            <p>Receive an offer within 24 hours, it&apos;s that simple!</p>
+          </div>
+        </div>
+        <div className="right">
+          <img src="/public/assets/homepage/Hero.svg" alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
