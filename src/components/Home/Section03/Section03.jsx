a750e34a588203996f469e944ba4563226a8f68c
Abhishek Jain <abhishek.j0407@gmail.com>
2023-08-26 14:19:26 +0530

    Section 3 added with styles

--- src/components/Home/Section03/Section03.jsx
+++ src/components/Home/Section03/Section03.jsx
@@ -1,8 +1,18 @@
 import './Section03.css'
+import Offer from './../Offer/Offer';
 
 const Section03 = () => {
   return (
-    <div>Section03</div>
+    <div id="section03">
+      <div className="container">
+        <div className="content">
+          <h3>Enter you home details and</h3>
+          <h2>Get an instant offer.</h2>
+          <p>Within 24 hours!</p>
+          <Offer />
+        </div>
+      </div>
+    </div>
   )
 }
 
